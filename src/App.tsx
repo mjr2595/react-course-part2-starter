9ac8a9dc0947d148d0c14b8a4aaf30df34003886
mjr2595 <michaeljohnraymond@gmail.com>
2024-02-11 22:46:12 -0600

    LoginStatus with authReducer

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
 import Counter from "./state-management/Counter";
+import LoginStatus from "./state-management/LoginStatus";
 import TaskList from "./state-management/TaskList";
 
 function App() {
-  return <TaskList />;
+  return <LoginStatus />;
 }
 
 export default App;
