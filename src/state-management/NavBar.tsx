24701a728851fe4be69fdd7ec02dcba5fdae1403
mjr2595 <michaeljohnraymond@gmail.com>
2024-02-15 23:48:35 -0600

    Refactor NavBar and update counter reset value

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,11 +1,10 @@
-import { useContext } from "react";
 import LoginStatus from "./auth/LoginStatus";
-import TasksContext from "./tasks/tasksContext";
 import useCounterStore from "./counter/store";
 
 const NavBar = () => {
-  const { tasks } = useContext(TasksContext);
-  const { counter } = useCounterStore();
+  const counter = useCounterStore((s) => s.counter);
+
+  console.log("Render NavBar");
 
   return (
     <nav className="navbar d-flex justify-content-between">
